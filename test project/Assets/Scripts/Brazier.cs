9e00a364d86df7b5c70d5576a16417b2a2c443fa
LAPTOP-DANNY\Danny <dvduist@hotmail.nl>
2018-05-13 11:40:36 +0200

    You can now light a brazier with fireball (and extinguish it with water)

---
+++ test project/Assets/Scripts/Brazier.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brazier : MonoBehaviour
+{
+    public bool Lid;
+    public GameObject Door;
+    private DoorController _door;
+
+    private void Start()
+    {
+        _door = Door.GetComponentInChildren<DoorController>();
+        if (Lid)
+        {
+            _door.Open();
+        }
+    }
+
+    public void Light()
+    {
+        Lid = true;
+        _door.Open();
+    }
+
+    public void Extinguish()
+    {
+        Lid = false;
+        _door.Close();
+    }
+}
