2eed8f62b86cb7e07236b2cfc805fba4be468f85
MY-LITTLE-CHAD\Jesse <dron20d@mail.ru>
2018-05-15 13:09:45 +0200

    Made lever turn light on and off, added a cooldown to it and also fixed the brazier with a light point

--- test project/Assets/Scripts/Activate.cs
+++ test project/Assets/Scripts/Activate.cs
@@ -9,6 +9,7 @@ public class Activate : MonoBehaviour
     public GameObject interactive;
     private DoorController _door;
     private ElevatorController _elevator;
+    private float _inProgress;
 
     private void Start()
     {
@@ -16,6 +17,18 @@ public class Activate : MonoBehaviour
             _door = interactive.GetComponentInChildren<DoorController>();
         else if (interactive.tag == "Elevator")
             _elevator = interactive.GetComponent<ElevatorController>();
+
+    }
+
+    void Update()
+    {
+        if (tag == "Lever")
+            if (_inProgress > 0)
+            {
+                _inProgress -= Time.deltaTime;
+                if (_inProgress < 0)
+                    _inProgress = 0;
+            }
     }
 
     public void Animation()
@@ -28,67 +41,31 @@ public class Activate : MonoBehaviour
 
     public void Action()
     {
+        if (tag == "Lever")
+        {
+            if (_inProgress > 0)
+                return;
+            _inProgress = 2;
+        }
+
+        //Door
         if (interactive.tag == "Door")
         {
-            //Check if we are using lever
-            if (tag == "Lever")
-            {
-                //If so make a delay for using it
-                if (!_door.InProgress)
-                {
-                    activated = !activated;
-                    _door.ToggleActive();
-                }
-            }
-            else
-            {
-                activated = !activated;
-                _door.ToggleActive();
-            }
+            activated = !activated;
+            _door.ToggleActive();
+
         }
+        //Elevator
         else if (interactive.tag == "Elevator")
         {
             activated = !activated;
             _elevator.ToggleACtive();
         }
+        //Light
+        else if (interactive.tag == "Light")
+        {
+            activated = !activated;
+            interactive.gameObject.SetActive(!interactive.gameObject.activeSelf);
+        }
     }
-
-
-    //public void OpenDoor()
-    //{
-    //    //Check if we are using lever 
-    //    if (tag == "Lever")
-    //    {
-    //        //If so make a delay for using it
-    //        if (!_door.InProgress)
-    //        {
-    //            activated = true;
-    //            _door.ChangeActivity();
-    //        }
-    //    }
-    //    else
-    //    {
-    //        activated = true;
-    //        _door.ChangeActivity();
-    //    }
-    //}
-
-    //public void CloseDoor()
-    //{
-    //    //Check if we are using lever 
-    //    if (tag == "Lever")
-    //    {
-    //        //If so make a delay for using it
-    //        if (!_door.InProgress)
-    //        {
-    //            activated = false;
-    //            _door.ChangeActivity();
-    //        }
-    //    }
-    //    else
-    //    {
-    //        activated = false;
-    //        _door.ChangeActivity();
-    //    }
-    //}
 }
