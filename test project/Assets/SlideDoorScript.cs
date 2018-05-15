625e6d723b1675fc2204924c5a163f1966599adc
Luuk <l-oudevrielink@hotmail.com>
2018-05-15 13:52:36 +0200

    Waterwheel

---
+++ test project/Assets/SlideDoorScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SlideDoorScript : MonoBehaviour {
+
+    private float topMovement = 0f;
+    private Dictionary<Vector3, float> Doors = new Dictionary<Vector3, float>();
+
+    private void Update()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, topMovement, transform.localPosition.z);
+    }
+
+    public void SetDoorPosition(Vector3 door, float doorPosition)
+    {
+
+        if (Doors.ContainsKey(door))
+        {
+            Doors[door] = doorPosition;
+        }
+        else
+        {
+            Doors.Add(door, doorPosition);
+        }
+        topMovement = Doors.Select(x => x.Value).Max();
+
+    }
+}
