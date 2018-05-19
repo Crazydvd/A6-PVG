14fbd767ea569fd7ed598664f506fc1cdb37dbec
LAPTOP-DANNY\Danny <dvduist@hotmail.nl>
2018-05-19 13:13:13 +0200

    fixed merge issues

--- test project/Assets/Scripts/MenuScripts/MenuScript.cs
+++ test project/Assets/Scripts/MenuScripts/MenuScript.cs
@@ -6,10 +6,6 @@ public class MenuScript : MonoBehaviour
 {
     public void CloseGame()
     {
-<<<<<<< HEAD
-=======
-        Debug.Log("Game quit");
->>>>>>> dd06b18490aac07e199e29190801a0530b6c0360
         Application.Quit();
     }
 }
