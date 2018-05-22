714e21cf571285452acebb84440c1af580b8f7d0
MY-LITTLE-CHAD\Jesse <dron20d@mail.ru>
2018-05-22 19:12:43 +0200

    Added god voice with their triggers (also post-processing)

---
+++ test project/Assets/Scripts/LevelManagement/ActivateGod.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateGod : MonoBehaviour
+{
+    public GameObject subtitle;
+    public AudioClip voiceLine;
+
+    public void ActivateVoice()
+    {
+        subtitle.SetActive(true);
+        subtitle.GetComponent<AudioSource>().PlayOneShot(voiceLine);
+        Destroy(subtitle, 15f);        
+    }
+}
