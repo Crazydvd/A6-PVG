9908694c8f8ce6dc18a59635fc028a043fd3d5c8
Luuk <l-oudevrielink@hotmail.com>
2018-05-23 17:17:50 +0200

    merge

---
+++ test project/Assets/Scripts/Activatables/TrapScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapScript : Activatable {
+
+    [SerializeField] private GameObject _dart;
+    [SerializeField] private float _speed = 1000f;
+
+    private bool _active;
+	
+	// Update is called once per frame
+	void Update () {
+        if (_active)
+        {
+            foreach(Transform child in transform.GetComponentInChildren<Transform>())
+            {
+                GameObject dart = Instantiate(_dart, child.position, child.rotation);
+                dart.GetComponent<Rigidbody>().AddForce(transform.forward * _speed);
+                Destroy(dart, 5f);
+            }
+            _active = false;
+        }
+	}
+
+    public override void ToggleActive()
+    {
+        _active = !_active;
+    }
+
+    public override void ToggleActive(bool pActive)
+    {
+        _active = pActive;
+    }
+}
---
+++ test project/Assets/Scripts/MenuScripts/PauseMenuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Time.timeScale > 0f)
+            {
+                PauseGame();
+            }
+            else
+            {
+                ResumeGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1.0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = false;
+    }
+}
